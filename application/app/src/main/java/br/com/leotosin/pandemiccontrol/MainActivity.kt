9d1cd39f56abbd2ad7c83bd05e0164f574c755a7
Leonardo Bertele Tosin <leob.tosin@hotmail.com>
2021-01-17 20:27:30 -0300

    Create the display counting and the main events

--- application/app/src/main/java/br/com/leotosin/pandemiccontrol/MainActivity.kt
+++ application/app/src/main/java/br/com/leotosin/pandemiccontrol/MainActivity.kt
@@ -2,10 +2,30 @@ package br.com.leotosin.pandemiccontrol
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun increaseOne(view: View)
+    {
+        var counter : TextView = findViewById(R.id.countingField)
+        counter.text = (counter.text.toString().toInt() + 1).toString()
+    }
+
+    fun decreaseOne(view: View)
+    {
+        var counter : TextView = findViewById(R.id.countingField)
+        counter.text = (counter.text.toString().toInt() - 1).toString()
+    }
+
+    fun reset(view: View)
+    {
+        var counter : TextView = findViewById(R.id.countingField)
+        counter.text = getString(R.string.startCounting)
     }
 }
